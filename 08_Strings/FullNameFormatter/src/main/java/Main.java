c740c7859af39f49e81163d4de783db9f9286887
Volga Mashchitskaya <Volgushka@list.ru>
2021-10-23 20:45:29 +0300

    part1

--- 08_Strings/FullNameFormatter/src/main/java/Main.java
+++ 08_Strings/FullNameFormatter/src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
@@ -10,9 +11,37 @@ public class Main {
       if (input.equals("0")) {
         break;
       }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
-    }
-  }
+        int count = 0;
+        int gapCount = 0;
+        for (char i : input.toCharArray()) {
+            if(Character.UnicodeBlock.of(i).equals(Character.UnicodeBlock.CYRILLIC) || i == '-')
+            {count = count + 1;}
+            else if(i == ' '){gapCount += 1;}
+    };
 
-}
+        if (count == input.length() - 2 && gapCount == 2) {
+          int num = 1;
+          String str = " ";
+
+          int beginIndexSurname = 0;
+          int endIndexSurname = input.indexOf(str);
+          String surname = input.substring(beginIndexSurname, endIndexSurname);
+
+          int beginIndexName = input.indexOf(str);
+          int endIndexName = input.indexOf(str, input.indexOf(str) + num);
+          ;
+          String name = input.substring(beginIndexName, endIndexName);
+
+          int beginIndexMiddleName = endIndexName + num;
+          int endIndexMiddleName = input.length();
+          String middleName = input.substring(beginIndexMiddleName, endIndexMiddleName);
+
+          System.out.println("Фамилия: " + surname.trim());
+          System.out.println("Имя: " + name.trim());
+          System.out.println("Отчество: " + middleName.trim());
+        }
+      else{System.out.println("Введенная строка не является ФИО");}
+        }
+    }
+
+  }
--- 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
@@ -2,8 +2,33 @@ public class Main {
 
   public static void main(String[] args) {
 
-    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-    //TODO: напишите ваш код, результат вывести в консоль
+    String line = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+
+    System.out.println(sumSalary(line));
+
   }
 
+  public static int sumSalary(String line) {
+
+    int result = 0;
+    String str = "рубл";
+
+    //   for (char i : line.toCharArray()) {
+    for (int i = 0; i < line.length(); i++) {
+      if (Character.isWhitespace(line.toCharArray()[i]) && Character.isDigit(line.toCharArray()[i + 1])) {
+
+        int beginIndex = line.indexOf(line.toCharArray()[i + 1]);
+        int endIndex = line.indexOf(str);
+
+        String salary = line.substring(beginIndex, endIndex);
+        result = result + Integer.parseInt(salary.trim());
+        line = line.replaceFirst(str, "****").replaceFirst(salary, ("*").repeat(endIndex - beginIndex));
+      }
+    }
+  return result;
 }
+}
+
+
+
+
