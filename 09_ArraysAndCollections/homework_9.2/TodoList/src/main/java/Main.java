698a5abc734fe36090fe281bff2fd72fc1c47dd0
Volga Mashchitskaya <Volgushka@list.ru>
2021-11-13 01:11:06 +0300

    my work

--- 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/Main.java
@@ -1,7 +1,48 @@
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//LIST, ADD, EDIT, DELETE.
+
 public class Main {
+
     private static TodoList todoList = new TodoList();
 
     public static void main(String[] args) {
-        // TODO: написать консольное приложение для работы со списком дел todoList
+
+        System.out.println("Какую операцию желаете произвести?: \n ADD (добавить) \n EDIT(редактировать)\n DELETE(удалить) \n LIST(вывести список дел)");
+        Scanner input = new Scanner(System.in);
+        String deal = input.nextLine();
+        String[] dealList = deal.split(" ");
+        String regex = "[0-9]+";
+
+        String first = dealList[0];
+        int second = 0;
+        String secondS = Integer.toString(second);
+        String third;
+        if (dealList[1].matches(regex)) {
+            second = Integer.parseInt(dealList[1]);
+            third = deal.substring(first.length() + secondS.length(), deal.length());}
+        else {third = deal.substring(first.length(), deal.length());}
+
+        if (first.equals("ADD") & second == 0) {
+            todoList.add(third);
+
+        } else if (first.equals("ADD") & second != 0) {
+            todoList.add(second, third);
+        }
+
+            else if (first.equals("EDIT")) {
+//            System.out.println("Дело " + "\"" + todoList.getTodos().get(second).strip() + "\"" + "заменено на" + "\"" + third.strip() + "\"");
+            todoList.edit(third, second);
+
+        } else if (first.equals("DELETE")) {
+            todoList.delete(second);
+        } else if (first.equals("LIST")) {
+            for (int i = 0; i < todoList.getTodos().size(); i++) {
+                System.out.println(i + "-" + todoList.getTodos().get(i));
+            }
+
+        }
     }
 }
--- 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/TodoList.java
+++ 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/TodoList.java
@@ -2,28 +2,53 @@ import java.util.ArrayList;
 
 public class TodoList {
 
+    ArrayList<String> mylist = new ArrayList<String>();
+
     public void add(String todo) {
+        mylist.add(todo);
+        System.out.println("Добавлено дело " + "\"" + todo.strip() + "\"");
         // TODO: добавьте переданное дело в конец списка
     }
 
     public void add(int index, String todo) {
+        int limit = mylist.size();
+        if (index < limit) {
+           mylist.add(index, todo);
+            System.out.println("Добавлено дело " + "\"" + todo.strip() + "\"");
+       }
+       else {mylist.add(limit,todo);
+       System.out.println("Добавлено дело " + "\"" + todo.strip() + "\"");
+        }
         // TODO: добавьте дело на указаный индекс,
         //  проверьте возможность добавления
     }
 
-    public void edit(String todo, int index) {
-        // TODO: заменить дело на index переданным todo индекс,
-        //  проверьте возможность изменения
-    }
+        public void edit(String todo, int index) {
+            int limit = mylist.size();
+            if (index < limit) {
+                mylist.remove(index);
+                mylist.add(index, todo);
+                System.out.println("Дело " + "\"" + mylist.get(index).strip() +
+                        "\"" + "заменено на" + "\"" + todo.strip() + "\"");
+            }
+
+        }
+    // TODO: заменить дело на index переданным todo индекс,
+            //  проверьте возможность изменения
 
     public void delete(int index) {
+                int limit = mylist.size();
+                if (index < limit) {
+
+                    System.out.println("Дело " + "\"" + mylist.get(index).strip() +"\"" + " удалено");
+                    mylist.remove(index);}
+                else {System.out.println("Дело с таким номером не существует");}
         // TODO: удалить дело находящееся по переданному индексу,
         //  проверьте возможность удаления дела
     }
 
     public ArrayList<String> getTodos() {
-        // TODO: вернуть список дел
-        return new ArrayList<>();
-    }
+        return mylist;
+        }
 
-}
+     }
